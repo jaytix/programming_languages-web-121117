6f2c7a4c25c1a2212af14ceca238b43c54bf2b3d
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 01:42:19 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,9 +7,9 @@ def reformat_languages(languages)
       styles[1].keys.each do |language|
         hash[language] = {}
         styles[1].values.each_with_index do |types, i|
-          #binding.pry
-          hash[language] = {types}
           binding.pry
+          hash[language] = {types}
+          #binding.pry
           #end
         end
       end
