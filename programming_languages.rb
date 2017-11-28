e7a801938547add36cab287ce221f84b78d09365
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 01:25:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,9 +5,9 @@ def reformat_languages(languages)
   languages_by_style.each do |styles|
     styles.each_with_index do |style, i|
       styles[1].keys.each do |language|
-      
+
         if language == :ruby
-        end
+        #end
       end
     end
   end
