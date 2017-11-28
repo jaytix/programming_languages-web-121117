7bdc3b1e579a215499b0d2067966cb42250d86c8
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 02:18:37 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,14 +3,14 @@ require 'pry'
 def reformat_languages(languages_by_style)
   hash = Hash.new
   languages_by_style.each do |styles|
-    styles.each do |style|
+    styles.each_with_index do |style, i|
       styles[1].each do |language, types|
         hash[language] = {}
           #binding.pry
           types.each do |type, value|
           binding.pry
           hash[language] = {type => value}
-          hash[language][:style] = style
+          hash[language][:style] = style[i]
           #end
         end
       end
