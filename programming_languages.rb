a9b1fee5ea9838e3812f4c841a18998e07d7a3d8
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 01:18:40 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def reformat_languages(languages)
   hash = Hash.new
-  languages_by_style.each do |style|
-    style.each do |language|
+  languages_by_style.each do |styles|
+    styles.each do |style|
       binding.pry
       if language == :ruby
       end
