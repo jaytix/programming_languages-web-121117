bf6512dde5d4a82146a8c4984f834fa58119c68d
Joshua Ticzon <ticzon.joshua@gmail.com>
2017-11-28 01:18:04 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,8 +4,9 @@ def reformat_languages(languages)
   hash = Hash.new
   languages_by_style.each do |style|
     style.each do |language|
+      binding.pry
       if language == :ruby
       end
     end
-  end    
+  end
 end
